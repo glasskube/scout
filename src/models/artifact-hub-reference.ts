06d3635be10920a528db3f9c39b1e3201568e4f5
Philip Miglinci <pmig@glasskube.eu>
2024-02-26 13:35:23 +0100

    refactor: declare attributes in constructor
    Signed-off-by: Philip Miglinci <pmig@glasskube.eu>

--- src/models/artifact-hub-reference.ts
+++ src/models/artifact-hub-reference.ts
@@ -1,12 +1,9 @@
 export class ArtifactHubReference {
-  readonly owner: string;
-  readonly raw: string;
-  readonly repo: string;
-
-  constructor(raw: string, owner: string, repo: string) {
-    this.raw = raw;
-    this.owner = owner;
-    this.repo = repo;
+  constructor(
+    readonly raw: string,
+    readonly owner: string,
+    readonly repo: string,
+  ) {
   }
 }
 
--- src/models/manifest-url.ts
+++ src/models/manifest-url.ts
@@ -1,18 +1,16 @@
 import {SemVer} from 'semver';
 
 export class ManifestUrl {
-  readonly owner: string;
-  readonly path: string;
-  readonly raw: string;
-  readonly repo: string;
   readonly semVer: SemVer;
 
-  // eslint-disable-next-line  max-params
-  constructor(raw: string, owner: string, repo: string, version: string, path: string) {
-    this.raw = raw;
-    this.owner = owner;
-    this.repo = repo;
+  constructor(
+    readonly raw: string,
+    readonly owner: string,
+    readonly repo: string,
+    version: string,
+    readonly path: string,
+  ) {
     this.semVer = new SemVer(version);
-    this.path = path;
   }
+  // eslint-disable-next-line  max-params
 }
