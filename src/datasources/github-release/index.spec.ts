9578cf0e650d558c884f360c9a679842016e0fb6
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-02-27 17:16:08 +0100

    build: fix tests
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- src/datasources/github-release/index.spec.ts
+++ src/datasources/github-release/index.spec.ts
@@ -1,9 +1,22 @@
-import * as assert from 'node:assert';
+import {expect} from 'chai';
+import {HttpResponse, http} from 'msw';
+import {setupServer} from 'msw/node';
 import {SemVer} from 'semver';
 
 import {getLatestRelease} from './index.js';
 
+const handler = http.get(
+  'https://api.github.com/repos/glasskube/glasskube/releases/latest',
+  // eslint-disable-next-line camelcase
+  () => HttpResponse.json({tag_name: '0.0.2'}),
+);
+
+const server = setupServer(handler);
+
 describe('datasources/github', () => {
+  before(() => server.listen());
+  after(() => server.close());
+
   it('should download latest tag from GitHub', async () => {
     const version = await getLatestRelease({
       owner: 'glasskube',
@@ -12,6 +25,8 @@ describe('datasources/github', () => {
       repo: 'glasskube',
       semVer: new SemVer('0.0.2'),
     });
-    assert.equal(version.version, new SemVer('0.0.2').version);
+
+    expect(handler.isUsed).to.be.true;
+    expect(version.version).to.eq('0.0.2');
   });
 });
